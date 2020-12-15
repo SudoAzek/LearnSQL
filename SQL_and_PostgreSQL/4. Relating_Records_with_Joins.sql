78af36ec12b5c9e39494ea62a50ea669cb6cfb34
SudoAzek <45741494+SudoAzek@users.noreply.github.com>
2020-12-15 15:03:12 +0900

    Update 4. Relating_Records_with_Joins.sql

--- SQL_and_PostgreSQL/4. Relating_Records_with_Joins.sql
+++ SQL_and_PostgreSQL/4. Relating_Records_with_Joins.sql
@@ -19,10 +19,59 @@ VALUES
 /* With a NULL value it succesfully inserts the row to the table but without any relation to a FOREIGN KEY; */
 
 
-/* Dangling keys, references 
-*/
-
+/* What happens if you try to delete a user when there are photos being referenced from the user? 
+Because of dangling keys or references the following error message will be shown: 
+update or delete on table "users" violates foreign key constraint "photos_user_id_fkey" on table "photos" */
 DELETE FROM
   users
 WHERE
   id = 1;
+
+
+/* Table of users to relate to the later to be created table of photos */
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  username VARCHAR(50)
+);
+
+/* Insert data into users table */
+INSERT INTO
+  users (username)
+VALUES
+  ('monohan93'),
+  ('pferrer'),
+  ('si93onis'),
+  ('99stroman');
+
+/* ---------------------- Create table of photos with user_id reference on condition ON DELETE CASCADE  */
+CREATE TABLE photos (
+  id SERIAL PRIMARY KEY,
+  url VARCHAR(200),
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE
+);
+
+/* Try inserting images into photos table */
+INSERT INTO
+  photos (url, user_id)
+VALUES
+  ('http://one.jpg', 4),
+  ('http://two.jpg', 1),
+  ('http://25.jpg', 1),
+  ('http://36.jpg', 1),
+  ('http://754.jpg', 2),
+  ('http://35.jpg', 3),
+  ('http://256.jpg', 4);
+
+/* Try deleting user with PRIMARY KEY = 1. Because we have defined ON DELETE CASCADE, 
+now we can delete related data in other tables of user with id = 1 */
+DELETE FROM
+  users
+WHERE
+  id = 1;
+
+/* Check whether if the date persists on the photos table related to the user with PRIMARY_KEY = 1 */
+SELECT
+  *
+FROM
+  photos;
+
