7014bd2ef159978c7ee199ef1f791266b3c438a1
SudoAzek <45741494+SudoAzek@users.noreply.github.com>
2020-12-23 15:16:52 +0900

    Update 9. MODIFYING_DATA.sql

--- MySQL/Tutorial/9. MODIFYING_DATA.sql
+++ MySQL/Tutorial/9. MODIFYING_DATA.sql
@@ -74,3 +74,65 @@ VALUES
 AUTO_INCREMENT column, it can get the id of the first inserted row only, not the id of the last inserted row. */
 SELECT LAST_INSERT_ID();
 
+/* *********************** INSERT INTO SELECT ***************************** */
+
+/* First, let's create a new table called suppliers. */
+CREATE TABLE suppliers (
+	supplierNumber INT AUTO_INCREMENT,
+	supplierName VARCHAR(50) NOT NULL,
+	phone VARCHAR(50),
+	addressLine1 VARCHAR(50),
+	addressLine2 VARCHAR(50),
+	city VARCHAR(50),
+	state VARCHAR(50),
+	postalCode VARCHAR(50),
+	country VARCHAR(50),
+	customerNumber INT,
+	PRIMARY KEY(supplierNumber)
+);
+
+/* Following query finds all customers who locate in California, USA. */
+SELECT
+	customerNumber,
+	customerName,
+	phone,
+	addressLine1,
+	addressLine2,
+	city,
+	state,
+	postalCode,
+	country
+FROM
+	customers
+WHERE
+	country = 'USA' AND
+	state = 'CA';
+
+/* Now, use the INSERT INTO... SELECT statement to insert customers who locate 
+in California USA from the customers table into the suppliers table. */
+INSERT INTO suppliers (
+	supplierName,
+	phone,
+	addressLine1,
+	addressLine2,
+	city,
+	state,
+	postalCode,
+	country,
+	customerNumber
+)
+SELECT
+	customerName,
+	phone,
+	addressLine1,
+	addressLine2,
+	city,
+	state,
+	postalCode,
+	country,
+	customerNumber
+FROM
+	customers
+WHERE
+	country = 'USA' AND
+	state = 'CA';
